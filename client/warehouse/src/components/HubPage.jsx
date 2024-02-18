bcd3617b5670f8eaeb8f866a061b146a878b375a
Dima <93082934+DmitryCarikov@users.noreply.github.com>
2024-02-18 20:38:53 +0300

    feat: add hub page

---
+++ client/warehouse/src/components/HubPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HubPage = () => {
+  return (
+    <div>
+      <h1>ХАБ ЦДО</h1>
+      <ul>
+        <li><Link to="/categories">Управление категориями</Link></li>
+        <li><Link to="/warehouse">Управление складами</Link></li>
+        <li><Link to="/suppliers">Управление поставщиками</Link></li>
+        <li><Link to="/storage-location">Управление метсами хранения</Link></li>
+        <li><Link to="/products">Управление товарами</Link></li>
+        <li><Link to="/checks">Управление проверками</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default HubPage;
