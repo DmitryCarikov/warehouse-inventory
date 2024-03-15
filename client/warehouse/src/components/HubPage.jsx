a53cf730ffa4e90d52c680a2d23b841c39a76654
DmitryCarikov <93082934+DmitryCarikov@users.noreply.github.com>
2024-03-15 13:57:05 +0300

    Update HubPage.jsx

--- client/warehouse/src/components/HubPage.jsx
+++ client/warehouse/src/components/HubPage.jsx
@@ -9,7 +9,7 @@ const HubPage = () => {
         <li><Link to="/categories">Управление категориями</Link></li>
         <li><Link to="/warehouses">Управление складами</Link></li>
         <li><Link to="/suppliers">Управление поставщиками</Link></li>
-        <li><Link to="/storage-location">Управление метсами хранения</Link></li>
+        <li><Link to="/storage-location">Управление местами хранения</Link></li>
         <li><Link to="/products">Управление товарами</Link></li>
         <li><Link to="/checks">Управление проверками</Link></li>
       </ul>
