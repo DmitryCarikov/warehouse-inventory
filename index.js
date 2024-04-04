c173676198bdf938cc895a47a4cefa5301fea058
DmitryCarikov <93082934+DmitryCarikov@users.noreply.github.com>
2024-04-04 13:03:44 +0300

    refactor: Change mongodb connection link

--- index.js
+++ index.js
@@ -42,14 +42,9 @@ import {
 } from './validations.js'
 
 mongoose
-    .connect('mongodb+srv://admin:Hesus2016@cluster0.vgtv5yo.mongodb.net/WareHouse')
+    .connect('mongodb+srv://dimacarikov:hehehe@cluster0.orkhxfy.mongodb.net/WareHouse')
     .then(() => console.log('DB OK'))
     .catch((err) => console.log('DB ERROR', err));
-
-// mongoose
-//     .connect('mongodb+srv://dimacarikov:hehehe@cluster0.orkhxfy.mongodb.net/WareHouse')
-//     .then(() => console.log('DB OK'))
-//     .catch((err) => console.log('DB ERROR', err));
 
 
 const app = express();
